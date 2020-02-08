39656ead1ca83f8f95d508ca0e909a7fb2a91967
FluffMasterX <kobi.malul@gmail.com>
2020-02-08 15:52:10 +0200

    SaVe

--- Assets/StaticObjects.cs
+++ Assets/StaticObjects.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class StaticObjects : MonoBehaviour
 {
+    public float GameTimeMultiplier = 1.0f;
+    public float GameTime;
     // Start is called before the first frame update
     public static Transform PPH;
     public StaticObjects instance;
     void Awake()
     {
         instance = this;
+        GameTime = Time.time;
     }
 
     private void Start() {
         PPH = GameObject.FindGameObjectWithTag("PPH").transform;
+    }
+
+    private void FixedUpdate() {
+        GameTime = (Time.time * GameTimeMultiplier);
     }
 
     // Update is called once per frame
