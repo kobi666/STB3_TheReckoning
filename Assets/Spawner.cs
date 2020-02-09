6a6d9f338a1f55683ab35b9cb3be35390639f14c
FluffMasterX <kobi.malul@gmail.com>
2020-02-09 15:35:28 +0200

    Succcesfull Spawning Exeperiment

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -9,9 +9,9 @@ public class Spawner : MonoBehaviour
    public GameObject PathsParentObject;
    [SerializeField]
 
-   List<GameObject> _splines;
+   List<GameObject> _splineGOs;
    public List<Vector2> _initialPoints;
-   List<Vector2> InitialPoints(List<GameObject> SplineGoList) {
+   List<Vector2> InitilizeInitPoints(List<GameObject> SplineGoList) {
        List<Vector2> list = new List<Vector2>();
        foreach (GameObject SplineGO in SplineGoList)
        {
@@ -20,11 +20,11 @@ public class Spawner : MonoBehaviour
        return list;
        }
    
-   public List<GameObject> Splines {
+   public List<GameObject> SplineGOs {
        
-       get => _splines;
+       get => _splineGOs;
        set {
-           _splines = value;
+           _splineGOs = value;
        }
        
    }
@@ -35,11 +35,34 @@ public class Spawner : MonoBehaviour
        E.GetComponent<BezierSolution.UnitWalker>().spline = _spline.GetComponent<BezierSolution.BezierSpline>();
    }
 
+    public IEnumerator SpawnEnemiesToSplineAtInterval (int numberOfEnemies, float intervalBetweenSpawns) {
+        for (int i = 0 ; i < numberOfEnemies ; i++) {
+            SpawnEnemyToSpline(TestEnemyPrefab, SplineGOs[0],_initialPoints[0]);
+            yield return new WaitForSeconds(intervalBetweenSpawns);
+        }
+        Debug.Log("Finished");
+        yield break;
+    }
+
+    public IEnumerator SpawnEnemiesToSplineAtInterval_RandomSpline (int numberOfEnemies, float intervalBetweenSpawns) {
+        
+        for (int i = 0 ; i < numberOfEnemies ; i++) {
+            int r = Random.Range(0,SplineGOs.Count);
+            SpawnEnemyToSpline(TestEnemyPrefab, SplineGOs[r],_initialPoints[r]);
+            yield return new WaitForSeconds(intervalBetweenSpawns);
+        }
+        Debug.Log("Finished");
+        yield break;
+    }
+
+
+
+
    private void Awake() {
        PathsParentObject = gameObject.transform.parent.gameObject;
        for (int i = 0 ; i <= PathsParentObject.transform.childCount-1 ; i++ ) {
            if (PathsParentObject.transform.GetChild(i).gameObject.tag == "Spline") {
-           Splines.Add(PathsParentObject.transform.GetChild(i).gameObject);
+           SplineGOs.Add(PathsParentObject.transform.GetChild(i).gameObject);
            }
        }
        
@@ -47,8 +70,10 @@ public class Spawner : MonoBehaviour
    }
 
    private void Start() {
-       _initialPoints = InitialPoints(Splines);
-       SpawnEnemyToSpline(TestEnemyPrefab,Splines[0],_initialPoints[0]);
+       _initialPoints = InitilizeInitPoints(SplineGOs);
+       //SpawnEnemyToSpline(TestEnemyPrefab,SplineGOs[0],_initialPoints[0]);
+       //StartCoroutine(SpawnEnemiesToSplineAtInterval(6, 2.0f));
+           StartCoroutine(SpawnEnemiesToSplineAtInterval_RandomSpline(20, 2.0f));
    }
 
    
