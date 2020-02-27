ebfacda1fa3f467eb5d4d52f2ccf7bd1f05b3d42
FluffMasterX <kobi.malul@gmail.com>
2020-02-27 20:24:04 +0200

    Life Bar complete!

--- Assets/LifeBarmanager.cs
+++ Assets/LifeBarmanager.cs
@@ -5,6 +5,7 @@ using System;
 
 public class LifeBarmanager : MonoBehaviour
 {
+    SpriteRenderer ParentSpriteRenderer;
     int maxLife;
     float initialX;
     UnitLifeManager Life;
@@ -22,18 +23,20 @@ public class LifeBarmanager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ParentSpriteRenderer = transform.parent.GetComponent<SpriteRenderer>();
         initialX = LifeBarMaskPositon.position.x;
         maxLife = gameObject.GetComponentInParent<PlayerUnitController2>().Data.HP;
         Life = gameObject.GetComponentInParent<PlayerUnitController2>().LifeManager;
         Life.damageTaken += UpdateLifeBar;
         LifeBarSize = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x;
-        //ResizeLifeBarToUnitSpriteSize();
+        ResizeLifeBarToUnitSpriteSize();
     }
 
     public void ResizeLifeBarToUnitSpriteSize() {
-        float ParentSpriteSize = GetComponentInParent<SpriteRenderer>().sprite.bounds.size.x * GetComponentInParent<Transform>().localScale.x;
-        float ratio = LifeBarSize / ParentSpriteSize;
+        float ParentSpriteSize = ParentSpriteRenderer.sprite.bounds.size.x * transform.parent.transform.localScale.x;
+        //Debug.Log("Parent Bound size " + ParentSpriteRenderer.sprite.bounds.size.x);
+        //Debug.Log("LIFE Bound size " + GetComponent<SpriteRenderer>().sprite.bounds.size.x);
+        float ratio =   ParentSpriteSize / LifeBarSize;
         Vector2 newScale = new Vector2(ratio, transform.localScale.y);
         gameObject.transform.localScale = newScale;
     }
@@ -44,6 +47,10 @@ public class LifeBarmanager : MonoBehaviour
         float hitPercentageOfLife = ((float)damageTaken / (float)maxLife);
         float amountToMove = (LifeBarSize * hitPercentageOfLife);
         LifeBarMaskPositon.Translate(Vector2.left * amountToMove);    
+    }
+
+    private void Update() {
+       
     }
 
     // Update is called once per frame
